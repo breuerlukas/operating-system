38cdeb39c50f3f5970904bd216576064e834a409
DerCoderLukas <lukas.dercoder@gmail.com>
2023-06-02 14:58:50 +0200

    Implemented color

---
+++ src/terminal/color.h
@@ -0,0 +1,23 @@
+#ifndef OPERATING_SYSTEM_COLOR_H
+#define OPERATING_SYSTEM_COLOR_H
+
+enum Color {
+  kBlack = 0,
+  kBlue = 1,
+  kGreen = 2,
+  kCyan = 3,
+  kRed = 4,
+  kMagenta = 5,
+  kBrown = 6,
+  kLightGray = 7,
+  kDarkGray = 8,
+  kLightBlue = 9,
+  kLightGreen = 10,
+  kLightCyan = 11,
+  kLightRed = 12,
+  kLightMagenta = 13,
+  kLightBrown = 14,
+  kWhite = 15
+};
+
+#endif //OPERATING_SYSTEM_COLOR_H
